85f21a8f14a08e549326776146f82463c4cfdfb2
Ali Tehrani <git@tehrani.xyz>
2025-04-04 16:45:51 -0500

    update

--- search_r1/search/retrieval_server.py
+++ search_r1/search/retrieval_server.py
@@ -73,6 +73,5 @@ if __name__ == "__main__":
     # Print the IP address of the machine
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
-    print(f"Server is running on IP address: {ip_address}")
     # 3) Launch the server. By default, it listens on http://127.0.0.1:8000
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=ip_address, port=8000)
