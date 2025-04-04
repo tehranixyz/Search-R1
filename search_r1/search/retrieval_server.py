5ab65232dfc1cb935767c5366bda85955871eb87
Ali Tehrani <git@tehrani.xyz>
2025-04-04 11:24:39 -0500

    remove args

--- search_r1/search/retrieval_server.py
+++ search_r1/search/retrieval_server.py
@@ -15,15 +15,6 @@ import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
-parser = argparse.ArgumentParser(description="Launch the local faiss retriever.")
-parser.add_argument("--index_path", type=str, default="/home/peterjin/mnt/index/wiki-18/e5_Flat.index", help="Corpus indexing file.")
-parser.add_argument("--corpus_path", type=str, default="/home/peterjin/mnt/data/retrieval-corpus/wiki-18.jsonl", help="Local corpus file.")
-parser.add_argument("--topk", type=int, default=3, help="Number of retrieved passages for one query.")
-parser.add_argument("--retriever_model", type=str, default="intfloat/e5-base-v2", help="Name of the retriever model.")
-
-args = parser.parse_args()
-
 #####################################
 # FastAPI server below
 #####################################
@@ -74,11 +65,7 @@ app = FastAPI()
 #    In real usage, you'd parse your CLI arguments or environment variables.
 config = Config(
     retrieval_method = "e5",  # or "dense"
-    index_path=args.index_path,
-    corpus_path=args.corpus_path,
-    retrieval_topk=args.topk,
     faiss_gpu=True,
-    retrieval_model_path=args.retriever_model,
     retrieval_pooling_method="mean",
     retrieval_query_max_length=256,
     retrieval_use_fp16=True,
